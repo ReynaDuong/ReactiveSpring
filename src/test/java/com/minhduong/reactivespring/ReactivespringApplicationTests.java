7e252811999a3e0c1f0244c6ad622da2a1858825
ReynaDuong <minhduong.md25@gmail.com>
2020-01-12 13:17:40 -0600

    Initial commit

---
+++ src/test/java/com/minhduong/reactivespring/ReactivespringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minhduong.reactivespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactivespringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
