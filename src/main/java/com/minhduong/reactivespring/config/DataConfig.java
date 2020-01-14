b9265916f2a28d342ce35aa856ff98495f5f7f47
ReynaDuong <minhduong.md25@gmail.com>
2020-01-14 16:18:50 -0600

    MongoDB DataConfig and ServiceImpl

--- src/main/java/com/minhduong/reactivespring/config/DataConfig.java
+++ src/main/java/com/minhduong/reactivespring/config/DataConfig.java
@@ -3,10 +3,13 @@ package com.minhduong.reactivespring.config;
 
 import com.mongodb.reactivestreams.client.MongoClient;
 import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.ReactiveMongoDatabaseFactory;
+import org.springframework.data.mongodb.core.ReactiveMongoOperations;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import org.springframework.data.mongodb.core.SimpleReactiveMongoDatabaseFactory;
 
 @Profile(value = "local")
@@ -15,7 +18,13 @@ import org.springframework.data.mongodb.core.SimpleReactiveMongoDatabaseFactory;
 public class DataConfig {
     public static final String DATEBASE_NAME = "reservations";
 
+    @Bean
     public ReactiveMongoDatabaseFactory mongoDatabaseFactory (MongoClient mongoClient) {
         return new SimpleReactiveMongoDatabaseFactory(mongoClient, DATEBASE_NAME);
+    }
+
+    @Bean
+    public ReactiveMongoOperations reactiveMongoOperations (ReactiveMongoDatabaseFactory reactiveMongoDatabaseFactory) {
+        return new ReactiveMongoTemplate(reactiveMongoDatabaseFactory);
     }
 }
---
+++ src/main/java/com/minhduong/reactivespring/service/ReservationService.java
@@ -0,0 +1,15 @@
+package com.minhduong.reactivespring.service;
+
+import com.minhduong.reactivespring.model.Reservation;
+import reactor.core.publisher.Mono;
+
+public interface ReservationService {
+
+    Mono<Reservation> getReservation(String id);
+
+    Mono<Reservation> createReservation(Mono<Reservation> reservationMono);
+
+    Mono<Reservation> updateReservation(String id, Mono<Reservation> reservationMono);
+
+    Mono<Reservation> deleteReservation(String id);
+}
