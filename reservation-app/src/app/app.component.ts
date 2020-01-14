36fa4ac27c8618b9f5f9a44b91a39763a75dc343
ReynaDuong <minhduong.md25@gmail.com>
2020-01-14 15:23:07 -0600

    angular directives

--- reservation-app/src/app/app.component.ts
+++ reservation-app/src/app/app.component.ts
@@ -3,18 +3,38 @@ import {FormControl, FormGroup} from "@angular/forms";
 import {HttpClient} from "@angular/common/http";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reservation-app';
+    title = 'reservation-app';
 
-  constructor(private http:HttpClient) {
-  }
+    constructor(private http:HttpClient) {
+    }
 
-  private baseUrl:string = 'http://localhost:8080';
+    private baseUrl:string = 'http://localhost:8080';
 
-  private reservationUrl:string = this.baseUrl + '/room/v1/reservation/';
+    private reservationUrl:string = this.baseUrl + '/room/v1/reservation/';
 
+    rooms: Room [];
+
+    ngOnInit() {
+        this.rooms = [new Room("127", "127", "150"),
+            new Room("138", "138", "160"),
+            new Room("254", "254", "200")];
+    }
+
+}
+
+export class Room {
+    id: string;
+    roomNumber: string;
+    price: string;
+
+    constructor(id: string, roomNumber: string, price: string) {
+        this.id = id;
+        this.roomNumber = roomNumber;
+        this.price = price;
+    }
 }
--- src/main/java/com/minhduong/reactivespring/config/DataConfig.java
+++ src/main/java/com/minhduong/reactivespring/config/DataConfig.java
@@ -1,13 +1,21 @@
 package com.minhduong.reactivespring.config;
 
+
+import com.mongodb.reactivestreams.client.MongoClient;
 import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.mongodb.ReactiveMongoDatabaseFactory;
+import org.springframework.data.mongodb.core.SimpleReactiveMongoDatabaseFactory;
 
 @Profile(value = "local")
 @Configuration
 @Import(EmbeddedMongoAutoConfiguration.class)
 public class DataConfig {
+    public static final String DATEBASE_NAME = "reservations";
 
+    public ReactiveMongoDatabaseFactory mongoDatabaseFactory (MongoClient mongoClient) {
+        return new SimpleReactiveMongoDatabaseFactory(mongoClient, DATEBASE_NAME);
+    }
 }
