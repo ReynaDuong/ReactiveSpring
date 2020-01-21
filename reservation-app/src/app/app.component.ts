bf0154f5ea663a20eb8c71dcb2e5e79bd7b17c46
ReynaDuong <minhduong.md25@gmail.com>
2020-01-20 23:51:10 -0600

    reservation.service.ts to send Http request

--- reservation-app/src/app/app.component.ts
+++ reservation-app/src/app/app.component.ts
@@ -1,30 +1,50 @@
 import { Component } from '@angular/core';
 import {FormControl, FormGroup} from "@angular/forms";
-import {HttpClient} from "@angular/common/http";
+import {ReservationService} from "./reservation.service";
+
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
     title = 'reservation-app';
 
-    constructor(private http:HttpClient) {
+    constructor(private reservationService: ReservationService) {
     }
 
-    private baseUrl:string = 'http://localhost:8080';
-
-    private reservationUrl:string = this.baseUrl + '/room/v1/reservation/';
-
     rooms: Room [];
+    roomSearchForm: FormGroup;
+    currentCheckInVal: string;
+    currentCheckOutVal: string;
+    currentPrice: number;
+    currentRoomNumber: number;
+
 
     ngOnInit() {
+        this.roomSearchForm = new FormGroup({
+            checkin: new FormControl(''),
+            checkout: new FormControl(''),
+            roomNumber: new FormControl('')
+        });
+
+        this.roomSearchForm.valueChanges.subscribe(form => {
+            this.currentCheckInVal = form.checkin;
+            this.currentCheckOutVal = form.checkout;
+
+            if (form.roomNumber) {
+                let roomValues: string[] = form.roomNumber.split('|');
+                this.currentRoomNumber = Number(roomValues[0]);
+                this.currentPrice = Number(roomValues[1]);
+            }
+        });
+
         this.rooms = [new Room("127", "127", "150"),
             new Room("138", "138", "160"),
             new Room("254", "254", "200")];
     }
-
 }
 
 export class Room {
